be492e398cb4cbd43cf72b980fda35da7208a695
geroalencar <angeloabianchi@gmail.com>
2020-04-24 18:42:47 +0200

    Calculator

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,32 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {CleanButton, DialButton} from "./DialButton";
+
+const Display = props => {
+  return <div className="App-Display">{props.content}</div>
+};
 
 function App() {
-  const [username, setUsername] = React.useState(undefined);
+  const [number1, setNumber1] = React.useState('');         //numero da esquerda do operador
+  const [number2, setNumber2] = React.useState('');         //numero da direita do operador
+  const [operation, setOperation] = React.useState('');     //operador
+  const [result, setResult] = React.useState('');
+  const addNumber = n => {
+      if (operation) {
+          setNumber2(number2 + '' + n);
+      } else {
+          setNumber1(number1 + '' + n);
+      }
+  }
+  const cleanNumber = () => {
+      if (number2) {
+          setNumber2('');
+      } else {
+          if(operation) {
+              setOperation('');
+          } else {
+              if (number1) {
+                  setNumber1('');
+              }
+            }
+        }
+  };
+
+  const solve=() => {
+      setResult(eval(number1 + operation + number2));
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome <code>{username}</code> to NDS.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Username</p>
-        <input onChange={event => setUsername(event.target.value)} type="text" />
-        <p>Email</p>
-        <input type="email" />
-        <p>Password</p>
-        <input type="password" />
-        <button disabled={username === ''}>SignUp</button>
-      </header>
-    </div>
+      <div className="Fondo">
+        <div className="App">
+          <CleanButton onClick={cleanNumber} />
+          <Display content={number1 + operation + number2} />
+          <Display content={result} />
+
+          <DialButton value={7} onClick={addNumber} />
+          <DialButton value={8} onClick={addNumber} />
+          <DialButton value={9} onClick={addNumber} />
+          <DialButton value="+" onClick={setOperation} />
+
+          <DialButton value={4} onClick={addNumber} />
+          <DialButton value={5} onClick={addNumber} />
+          <DialButton value={6} onClick={addNumber} />
+          <DialButton value="-" onClick={setOperation} />
+
+          <DialButton value={1} onClick={addNumber} />
+          <DialButton value={2} onClick={addNumber} />
+          <DialButton value={3} onClick={addNumber} />
+          <DialButton value="*" onClick={setOperation} />
+
+          <DialButton value={0} onClick={addNumber} />
+          <DialButton value="." onClick={addNumber} />
+          <button onClick={solve}>=</button>
+          <DialButton value="/" onClick={setOperation} />
+        </div>
+      </div>
   );
 }
 
